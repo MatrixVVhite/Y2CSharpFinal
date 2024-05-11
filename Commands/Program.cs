78bf05f3d2e85e02d593a6576d456d4cc0981cb3
MatrixVVhite <yotamoscohen@gmail.com>
2024-05-11 18:47:37 +0300

    Added player directed movement

--- Commands/Program.cs
+++ Commands/Program.cs
@@ -10,10 +10,16 @@ namespace Commands
         {
             RenderingEngine tmr = new RenderingEngine(8, 8);
             TileMap myTileMap = new TileMap(8, 8);
-            TileObject to2 = new TileObject("P", new Position[] { new Position(0, 0), new Position(0, 0) }, new Position(2, 2), ConsoleColor.Red);
-            to2.Positions[0] = new Position(to2.CurrentPos.X + 1, to2.CurrentPos.Y + 1);
-            to2.Positions[1] = new Position(to2.CurrentPos.X - 1, to2.CurrentPos.Y + 1);
-            myTileMap.InsertObjectToMap(to2, to2.CurrentPos);
+            Player p1 = new Player(0, ConsoleColor.Red, 1, 1);
+            TileObject to2 = new TileObject("P", new Position[] { new Position(0, 0), new Position(0, 0) }, new Position(2, 2), p1);
+            to2.Positions[0] = new Position(to2.CurrentPos.X + 1 * to2.Owner.MovesToX, to2.CurrentPos.Y + 1 * to2.Owner.MovesToY);
+            to2.Positions[1] = new Position(to2.CurrentPos.X - 1 * to2.Owner.MovesToX, to2.CurrentPos.Y + 1 * to2.Owner.MovesToY);
+            List<TileObject> objectList = new List<TileObject>() { to2 };
+            p1.AddPieces(objectList);
+            foreach (TileObject obj in p1.PiecesOwned)
+            {
+                myTileMap.InsertObjectToMap(to2, to2.CurrentPos);
+            }
             tmr.UpdateAndRender(myTileMap);
             string command = Console.ReadLine();
             CommandHandler.DiagnoseCommand(command, myTileMap, tmr);
--- CoreEngineHierarchy/TileObject.cs
+++ CoreEngineHierarchy/TileObject.cs
@@ -10,6 +10,7 @@ namespace CoreEngineHierarchy
         public string TileObjectChar { get; set; }
         public Position[] Positions { get; set; }
         public Position CurrentPos { get; set; }
+        public Player Owner { get; set; }
         public ConsoleColor Color { get; set; }
 
         /// <summary>
@@ -18,7 +19,7 @@ namespace CoreEngineHierarchy
         public List<Position> PossiblePositions { get; internal set; }
 
         /// <summary>
-        /// Dictionary with the possibale possitions on the board
+        /// Creates a playerless tileObject, commonly used for empty spaces
         /// </summary>
         public TileObject(string tileObjectChar, Position[] tileObjrctPositions, Position startingPos, ConsoleColor color)
         {
@@ -33,7 +34,25 @@ namespace CoreEngineHierarchy
             }
 
             CurrentPos = startingPos;
+        }
 
+        /// <summary>
+        /// Creates a tileObject with a player reference
+        /// </summary>
+        public TileObject(string tileObjectChar, Position[] tileObjrctPositions, Position startingPos, Player owner)
+        {
+            TileObjectChar = tileObjectChar;
+            Positions = tileObjrctPositions;
+            PossiblePositions = new List<Position>();
+            Owner = owner;
+            Color = owner.PiecesColor;
+
+            foreach (Position pos in Positions)
+            {
+                PossiblePositions.Add(pos);
+            }
+
+            CurrentPos = startingPos;
         }
 
     }
